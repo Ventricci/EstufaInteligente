06db7a760ee96f8181905dcf56916d1f77eb986d
Augusto Lázaro <93953255+augustolr06@users.noreply.github.com>
2023-11-08 16:46:31 -0300

    refactor: change port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 
 export const mqttClient = new MqttHandler();
 mqttClient.connect();
