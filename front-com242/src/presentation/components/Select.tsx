2af4ddbba3dc37f23e15fdcfffec8e79ca333b23
Thais Souza <thaisdesouza311@gmail.com>
2023-11-20 17:53:33 -0300

    refactor: Props added.

--- front-com242/src/presentation/components/Select.tsx
+++ front-com242/src/presentation/components/Select.tsx
@@ -1,10 +1,9 @@
 import * as React from "react";
-import { Theme, useTheme } from "@mui/material/styles";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Select, { SelectProps } from "@mui/material/Select";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -17,34 +16,13 @@ const MenuProps = {
   },
 };
 
-function getStyles(name: string, personName: string[], theme: Theme) {
-  return {
-    fontWeight:
-      personName.indexOf(name) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium,
-  };
-}
-
 interface PropTypes {
   name: string;
   select: string[];
+  onChange: SelectProps<string>["onChange"];
 }
 
-const MultipleSelect: React.FC<PropTypes> = ({name, select}) => {
-  const theme = useTheme();
-  const [personName, setPersonName] = React.useState<string[]>([]);
-
-  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
-    const {
-      target: { value },
-    } = event;
-    setPersonName(
-      // On autofill we get a stringified value.
-      typeof value === "string" ? value.split(",") : value
-    );
-  };
-
+const MultipleSelect: React.FC<PropTypes> = ({ name, select, onChange }) => {
   return (
     <div>
       <FormControl sx={{ m: 1, width: 300 }}>
@@ -52,25 +30,19 @@ const MultipleSelect: React.FC<PropTypes> = ({name, select}) => {
         <Select
           labelId="demo-multiple-name-label"
           id="demo-multiple-name"
-          value={personName}
-          onChange={handleChange}
           input={<OutlinedInput label="Name" />}
           MenuProps={MenuProps}
+          onChange={onChange}
         >
           {select.map((name) => (
-            <MenuItem
-              key={name}
-              value={name}
-              style={getStyles(name, personName, theme)}
-            >
+            <MenuItem key={name} value={name}>
               {name}
             </MenuItem>
           ))}
-          <div></div>
         </Select>
       </FormControl>
     </div>
   );
-}
+};
 
-export default MultipleSelect
+export default MultipleSelect;
